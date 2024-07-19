6f2e09873ba35e45c6af6c85ac95205a858665c6
DARSHAN VAGHELA <vdarshan613@gmail.com>
2024-07-19 22:49:25 +0530

    Change

--- src/config/env-vars.js
+++ src/config/env-vars.js
@@ -1,5 +1,5 @@
 module.exports = {
-  aws_access_key: process.env.AWS_ACCESS_KEY,
-  aws_secret_key: process.env.AWS_SECRET_KEY,
-  bucket: process.env.AWS_S3_BUCKET,
+  aws_access_key: process.env.BE_AWS_ACCESS_KEY,
+  aws_secret_key: process.env.BE_AWS_SECRET_KEY,
+  bucket: process.env.BE_AWS_S3_BUCKET,
 };
